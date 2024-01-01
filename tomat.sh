0166e9170a638c9d056778f28c470ccc4156dfd6
DoctorCorto <45grn6meov@runbox.email>
2024-01-01 16:01:35 +0100

    initial tomat script

---
+++ tomat.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#
+#
+
+sleep 1500 && notify-send "break" && sleep 3 && zenity --warning --text="25 minutes passed" && sleep 300 && notify-send "Break over!" && sleep 3 && zenity --warning --text="Break is over. Time to work again!"
+
+
