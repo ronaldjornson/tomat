3435445bd4b51723c2cda60d05fa54f11b67ff8c
DoctorCorto <45grn6meov@runbox.email>
2024-01-01 18:56:24 +0100

    Changed indentation for 'tomat'

--- tomat.sh
+++ tomat.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 #
-#
+# A Script to make alerts for pomodoro sessions.
 #
 
-sleep 1500 && notify-send "break" && sleep 3 && zenity --warning --text="25 minutes passed" && sleep 300 && notify-send "Break over!" && sleep 3 && zenity --warning --text="Break is over. Time to work again!"
+sleep 1500 \
+  && notify-send "break" \
+  && sleep 3 \
+  && zenity --warning --text="25 minutes passed" \
+  && sleep 300 \
+  && notify-send "Break over!" \
+  && sleep 3 \
+  && zenity --warning --text="Break is over. Time to work again!"
 
 
